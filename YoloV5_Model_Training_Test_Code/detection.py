e8403853e5ca042b432423980ef0aad3e547f276
Ragib Yasar Rahman <58731753+Ragib301@users.noreply.github.com>
2024-10-26 12:13:46 +0600

    Update detection.py

--- YoloV5_Model_Training_Test_Code/detection.py
+++ YoloV5_Model_Training_Test_Code/detection.py
@@ -4,21 +4,15 @@ import sys
 import pathlib
 pathlib.PosixPath = pathlib.WindowsPath
 
-model_path = r'best.pt'
-model = torch.hub.load('ultralytics/yolov5', 'custom',
-                       path=model_path)
-model.conf = 0.8  # Confidence threshold (0-1)
+model_path = 'best.pt'
+model = torch.hub.load('ultralytics/yolov5', 'custom', path=model_path)
+model.conf = 0.7  # Confidence threshold (0-1)
 model.iou = 0.2
 
-def detect(image):
-    frame = image
-    frame = cv2.resize(frame, (1280, 720))
+def detect(frame):
+    frame = cv2.resize(frame, (960, 540))
     detections = model(frame[..., ::-1])
     results = detections.pandas().xyxy[0].to_dict(orient="records")
-
-    if len(results) > 0:
-        print(results)
-        print('\n')
 
     for result in results:
         cs = result['name']
@@ -41,9 +35,7 @@ def detect(image):
         cv2.destroyAllWindows()
         sys.exit()
 
-
 if __name__ == "__main__":
-    # vid_location = r'/media/ragib314/Ragib/Computer Vision/Oil Spill Detection/Test.mp4'
     webcam = cv2.VideoCapture('vid1.mp4')
     while True:
         ret, im = webcam.read()
